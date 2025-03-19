1583159d60641132ab98fca40f5d66628b91eb17
mayank543 <124419530+mayank543@users.noreply.github.com>
2025-03-19 22:26:31 +0530

    Update route.ts

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary }  from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
